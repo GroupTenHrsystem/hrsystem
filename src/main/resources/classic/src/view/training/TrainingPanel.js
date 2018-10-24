53cafbb9a1bf7b702b9ded53faca8c8395390cc7
mkyyajj <1123598303@qq.com>
2018-10-24 16:02:16 +0800

    删掉了一些不必要的东西

--- src/main/resources/classic/src/view/training/TrainingPanel.js
+++ src/main/resources/classic/src/view/training/TrainingPanel.js
@@ -34,11 +34,6 @@ Ext.define('Admin.view.training.TrainingPanel', {
                     items: [
                     	{xtype: 'button', iconCls: 'x-fa fa-close' ,handler: 'openEditWindow'}
                     ]
-                },
-                {xtype: 'actioncolumn',cls: 'content-column', width: 120,text: '清除通过记录',tooltip: '清除记录 ',
-                    items: [
-                    	{xtype: 'button', iconCls: 'x-fa fa-close' ,handler: 'deleteOneRow'}
-                    ]
                 }
             ],
             tbar: [{
